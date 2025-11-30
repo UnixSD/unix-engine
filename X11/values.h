d34a33c6862b139085f298b0ad8f16daae13ba83
UnixSD <zaharkovtonuk7@gmail.com>
2025-11-30 14:10:54 +0200

    Update values.h

--- X11/values.h
+++ X11/values.h
@@ -16,47 +16,46 @@
    along with Unix Engine.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define UXFRAME_WINDOW
-#ifndef UXFRAME_WINDOW
-
+#pragma once
 #include <cstdlib>
 
 #if defined(_WIN32)
 #       define UXFRAME_OS_WIN
 #       define BASE_GRAPHIC dwm
-#       include <Windows.h>
 #elif defined(__APPLE__)
 #       define UXFRAME_OS_APPLE
 #       define BASE_GRAPHIC qartz
 #elif defined(__linux__)
 #       define UXFRAME_OS_BASE
-#       if (setenv("GRAPH_MODE", NULL, 1) == "xcb")
-#               define BASE_GRAPHIC xcb
-#       else
-#               define BASE_GRAPHIC wayland
-#       endif // setenv
+        const char *val = getenv("GRAPH_MODE");
+
+        if (val != nullptr && strcmp(val, "xcb") == 1)
+            #define BASE_GRAPHIC xcb
+        else if (val != nullptr && strcmp(val, "wayland") == 1)
+            #define BASE_GRAPHIC wayland 
 #endif // _WIN32
 
-typedef signed int Wid;
-typedef long int  WSize;
-typedef typename signed WTitle;
+
 
 #ifndef BASE_GRAPHIC == xcb
 #       include <X11/Xlib.h>
 #elif defined(BASE_GRAPHIC) == wayland
 #       include <wayland-client.h>
 #       include <wayland-server.h>
-#       include <wayland-protocols.h>
+
+        struct wl_registry* registry;
+        struct wl_compositor* compositor;
+        struct wl_seat* seat;
+        struct wl_display* display;
+   
 #elif defined(BASE_GRAPHIC) == qartz
 #       ifndef __OBJC__ // Enable Objective-C
 #               include <CoreGraphics/CoreGraphics.h>
 #       endif // __OBJC__
 #endif // BASE_GRAPHIC
+   
+typedef signed int Wid;
+typedef static signed int WSize;
+typedef char WTitle;
 
-namespace Propetry
-{
-        namespace Cursor {}
-        namespace Display {}
-        namespace Window {}
-}
-#endif //UXFRAME_WINDOW
+typedef signed int Display;
